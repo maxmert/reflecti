4e654d8b20e2d8d579c04813a3816d747565711e
Maxim Vetrenko <maxim.vetrenko@deliveryhero.com>
2016-10-15 21:50:31 +0200

    Hotfix :'( Exports fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 export { default as Action } from './action';
 export { default as createStore } from './store';
 export { default as emitterMiddleware } from './middlewares/emitter';
-export { default as emitterObservable } from './middlewares/observable';
-export { default as singleObservable } from './middlewares/single';
-export { default as storageObservable } from './middlewares/storage';
-export { default as timeMashineObservable } from './middlewares/time-mashine';
+export { default as observableMiddleware } from './middlewares/observable';
+export { default as singleMiddleware } from './middlewares/single';
+export { default as storageMiddleware } from './middlewares/storage';
+export { default as timeMashineMiddleware } from './middlewares/time-mashine';
